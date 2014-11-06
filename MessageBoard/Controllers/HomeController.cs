0a983d9b1636dd0c86b1bca2289f8d349016e126
Edward <edward@inchbuie.net>
2014-11-06 13:07:13 -0600

    added initial mail service

--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MessageBoard.Models;
+using MessageBoard.Services;
 
 namespace MessageBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private IMailService _mailSvc;
+
+        public HomeController()
+        {
+            _mailSvc = new MailService();
+        }
+        public HomeController(IMailService mailSvc)
+        {
+            _mailSvc = mailSvc;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -33,6 +45,18 @@ namespace MessageBoard.Controllers
         [HttpPost]
         public ActionResult Contact(ContactModel model)
         {
+            var msg = string.Format("Comment From: {1}{0}Email:{2}{0}Email:{3}{0}Email:{4}{0}",
+                Environment.NewLine,
+                model.Name,
+                model.Email,
+                model.Website,
+                model.Comment);
+
+            ViewBag.MailSent = _mailSvc.SendMail("noreply@inchbuie.net",
+                "edward@inchbuie.net",
+                "website contact",
+                msg);
+
             return View();
         }
     }
