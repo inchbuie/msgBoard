5eec2fdf8c0b52f58a205779fc3d5bb25ba152ce
Edward <edward@inchbuie.net>
2014-11-17 13:27:16 -0600

    AngularJS basics - homeIndexController

---
+++ MessageBoard/Scripts/home-index.js
@@ -0,0 +1,26 @@
+﻿// home-index.js
+function homeIndexController($scope) {
+    console.log("inside the home index controller");
+
+    $scope.name = "Dennis D. Menace";
+    $scope.data = [
+        {
+            title: "first message",
+            body: "hello, how art thou?",
+            created: "11/11/2014"
+        },
+        {
+            title: "second message",
+            body: "Good Evening, Ladies and Gentlemen",
+            created: "11/12/2014"
+        },
+        {
+            title: "third message",
+            body: "what up homez?",
+            created: "11/13/2014"
+        }
+
+    ];
+}
+
+angular.module("messageBoard", []).controller("homeIndexController", homeIndexController);
