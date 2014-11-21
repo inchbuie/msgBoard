74a8ac02bb5dbeab9833dd8efe9132730ae94051
Edward <edward@inchbuie.net>
2014-11-21 10:40:05 -0600

    js cleanup

--- MessageBoard/Scripts/home-index.js
+++ MessageBoard/Scripts/home-index.js
@@ -1,60 +1,60 @@
-﻿// home-index.js
+﻿//home-index.js
 
-var module = angular.module("homeIndex", ['ngRoute']);
+var module = angular.module( "homeIndex", [ "ngRoute" ] );
 
 //configure client-side routing with angular
 module.config([
     "$routeProvider",
-    function ($routeProvider) {
-        $routeProvider.when("/", {
-            controller: topicsController, // when controller looks like this
-            templateUrl: "/templates/topicsView.html" // use this url fragment to load html template from server
-        });
+    function( $routeProvider ){
+        $routeProvider.when( "/", {
+            controller : topicsController, // when controller looks like this
+            templateUrl : "/templates/topicsView.html" // use this url fragment to load html template from server
+        } );
 
-        $routeProvider.when("/newmessage", {
-            controller: newTopicController,
-            templateUrl: "templates/newTopicView.html"
-        });
+        $routeProvider.when( "/newmessage", {
+            controller : newTopicController,
+            templateUrl : "templates/newTopicView.html"
+        } );
 
-        $routeProvider.when("/message/:id", {
-            controller: singleTopicController,
-            templateUrl: "templates/singleTopicView.html"
-        });
+        $routeProvider.when( "/message/:id", {
+            controller : singleTopicController,
+            templateUrl : "templates/singleTopicView.html"
+        } );
 
         $routeProvider.otherwise({
-            redirectTo: "/"
-        }); //default route
+            redirectTo : "/"
+        } ); //default route
     }
-]);
+] );
 
 // create an AngularJS service for inter-controller communication
 module.factory("dataService", [
     "$http",
     "$q",
-    function ($http, $q) { //callback for service
+    function($http, $q) { //callback for service
         //local members
         var _topics = [];
         var _isInitialized = false;
 
-        var _dataLoaded = function () {
+        var _dataLoaded = function() {
             return _isInitialized;
-        }
+        };
 
-        var _getTopics = function () {
+        var _getTopics = function() {
 
             //create deferral object to expose promise (using Angular's magic $q, which was injected)
             var deferred = $q.defer();
 
-            $http.get("/api/v1/topics?includeReplies=true")
+            $http.get( "/api/v1/topics?includeReplies=true" )
                .then(
-                   function (result) {
+                   function( result ) {
                        //succesful
-                       angular.copy(result.data, _topics);
+                       angular.copy( result.data, _topics );
                        // signal that data has been initialized
                        _isInitialized = true;
                        deferred.resolve();
                    },
-                   function () {
+                   function() {
                        //error
                        deferred.reject();
                    });
@@ -64,19 +64,19 @@ module.factory("dataService", [
         };
 
 
-        var _addTopic = function (newTopic) {
+        var _addTopic = function( newTopic ) {
             var deferred = $q.defer();
 
-            $http.post("/api/v1/topics", newTopic)
+            $http.post( "/api/v1/topics", newTopic )
                 .then(
-                    function (result) {
+                    function(result) {
                         //succesful
                         var savedTopic = result.data;
                         // merge with existing list of topics
-                        _topics.splice(0, 0, savedTopic);
-                        deferred.resolve(savedTopic);
+                        _topics.splice( 0, 0, savedTopic );
+                        deferred.resolve( savedTopic );
                     },
-                    function () {
+                    function() {
                         //error
                         deferred.reject();
                     });
@@ -84,12 +84,12 @@ module.factory("dataService", [
             return deferred.promise;
         };
 
-        function _findTopic(id) {
+        function _findTopic( id ) {
             //assumes _dataLoaded==true
             var found = null;
 
-            $.each(_topics, function (i, item) {
-                if (item.id == id) {
+            $.each(_topics, function( i, item ) {
+                if ( item.id === id ) {
                     found = item;
                     return false;
                 }
@@ -98,23 +98,23 @@ module.factory("dataService", [
             return found;
         }
 
-        var _getTopicById = function (id) {
+        var _getTopicById = function( id ) {
             var deferred = $q.defer();
 
             if (_dataLoaded()) {
-                var topic = _findTopic(id);
+                var topic = _findTopic( id );
                 if (topic) {
-                    deferred.resolve(topic);
+                    deferred.resolve( topic );
                 } else {
                     deferred.reject();
                 }
             } else {
                 _getTopics()
-                    .then(function () {
+                    .then(function() {
                         //succesful
-                        var topic = _findTopic(id);
-                        if (topic) {
-                            deferred.resolve(topic);
+                        var topic = _findTopic( id );
+                        if ( topic ) {
+                            deferred.resolve( topic );
                         } else {
                             deferred.reject();
                         }
@@ -126,15 +126,34 @@ module.factory("dataService", [
             }
 
             return deferred.promise;
-        }
+        };
+
+        var _saveReply = function( topic, newReply ) {
+            var deferred = $q.defer();
+
+            $http.post( "/api/v1/topics/" + topic.id + "/replies", newReply )
+              .then(function( result ) {
+                  // success
+                  if (topic.replies === null) topic.replies = [];
+                  topic.replies.push( result.data );
+                  deferred.resolve( result.data );
+              },
+              function () {
+                  // error
+                  deferred.reject();
+              });
+
+            return deferred.promise;
+        };
 
         // must return object representing the service, exposing private members
         return {
-            topics: _topics,
-            getTopics: _getTopics,
-            addTopic: _addTopic,
-            dataLoaded: _dataLoaded,
-            getTopicById: _getTopicById
+            topics : _topics,
+            getTopics : _getTopics,
+            addTopic : _addTopic,
+            dataLoaded : _dataLoaded,
+            getTopicById : _getTopicById,
+            saveReply : _saveReply
         };
     }
 ]);
@@ -146,14 +165,14 @@ var topicsController = [
     "$scope",
     "$http",
     "dataService",
-    function ($scope, $http, dataService) {
+    function( $scope, $http, dataService ) {
         //console.log("inside the topics controller");
 
         $scope.dataSvc = dataService;
         $scope.isBusy = false;
 
         //only call data service if this is the 1st time & data not already loaded
-        if (dataService.dataLoaded() == false) {
+        if (dataService.dataLoaded() === false) {
             $scope.isBusy = true;
             dataService.getTopics()
                 .then(
@@ -162,7 +181,7 @@ var topicsController = [
                     },
                     function () {
                         //error
-                        console.log("error loading topics");
+                        console.log( "error loading topics" );
                     })
                     .then(function () {
                         // after either error or success
@@ -175,29 +194,30 @@ var topicsController = [
 
 var newTopicController = [
     "$scope",
+    "dataService",
     "$http",
     "$window",
-    function ($scope, $http, $window) {
+    function( $scope, dataService, $http, $window ) {
         $scope.newTopic = {};
 
-        $scope.save = function () {
+        $scope.save = function() {
             //console.log("save() called for " + $scope.newTopic.title);
 
-            dataService.addTopics($scope.newTopic)
+            dataService.addTopics( $scope.newTopic )
              .then(
-                    function (result) {
+                    function(result) {
                         //succesful
                         $window.location = "#/";
                     },
-                    function () {
+                    function() {
                         //error
-                        console.log("error saving new topic");
+                        console.log( "error saving new topic" );
                     })
-                    .then(function () {
+                    .then(function() {
                         // after either error or success
                         $scope.isBusy = false;
                     });
-        }
+        };
     }
 ];
 
@@ -206,24 +226,32 @@ var singleTopicController = [
     "dataService",
     "$window",
     "$routeParams",
-    function ($scope, dataService, $window, $routeParams) {
+    function( $scope, dataService, $window, $routeParams ) {
         $scope.topic = null;
         $scope.newReply = {};
 
         dataService.getTopicById($routeParams.id)
              .then(
-                function (result) {
+                function( result ) {
                     //succesful
                     $scope.topic = result;
                 },
-                function () {
+                function() {
                     //error, return to main page
                     $window.location = "#/";
 
                 });
 
-        $scope.addReply = function () {
-
+        $scope.addReply = function() {
+            dataService.saveReply($scope.topic, $scope.newReply)
+              .then(function() {
+                  // success
+                  $scope.newReply.body = "";
+              },
+              function() {
+                  // error
+                  alert("Could not save the new reply");
+              });
         };
     }
 ];
